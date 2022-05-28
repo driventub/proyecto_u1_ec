08e2a84c40dcebca6e98a15c2311469523ea6242
driven <aldazpaulo@gmail.com>
2022-05-27 20:19:22 -0500

    Segundo Cambio

--- src/main/java/com/uce/avanzada/proyecto_u1_ec/ProyectoU1EcApplication.java
+++ src/main/java/com/uce/avanzada/proyecto_u1_ec/ProyectoU1EcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoU1EcApplication implements CommandLineRunner{
-
+// Cambio
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1EcApplication.class, args);
 	}
